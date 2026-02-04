d3908f83ecea7aa5108643a40393bfe746e3d008
nevilpaul12 <nevilpaul1234@gmail.com>
2026-02-04 15:44:36 +0530

    Fix: capture page

--- app/api/document/uploader/route.ts
+++ app/api/document/uploader/route.ts
@@ -1,9 +1,13 @@
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { s3Client } from "../../../../utils/s3";
 import { randomUUID } from "crypto";
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
-export async function uploadToS3(fileBuffer: Buffer, mimeType: string, folder = "uploads") {
+export async function uploadToS3(
+  fileBuffer: Buffer,
+  mimeType: string,
+  folder = "uploads",
+) {
   const key = `${folder}/${randomUUID()}`;
 
   await s3Client.send(
@@ -14,11 +18,10 @@ export async function uploadToS3(fileBuffer: Buffer, mimeType: string, folder = 
       ContentType: mimeType,
       ACL: "private", // keep it secure
       ServerSideEncryption: "AES256", // or "aws:kms" if using KMS
-    })
+    }),
   );
 
-  return NextResponse.json(`https://${process.env.S3_BUCKET_NAME}.s3.amazonaws.com/${key}`);
+  return NextResponse.json(
+    `https://${process.env.S3_BUCKET_NAME}.s3.amazonaws.com/${key}`,
+  );
 }
-
-
-
